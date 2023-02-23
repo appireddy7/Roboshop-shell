12c14c077004e8f692266ce9fcb674f460baa704
Appi Reddy <appireddy.777@outlook.com>
2023-02-23 20:42:35 +0530

    frontend in roboshop

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,11 @@
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+systemctl enable nginx
+systemctl start nginx
 
+# Roboshop config is not configured
+
+# if any command is errored or failed we need to stop the script
