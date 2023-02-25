b7787330e665fd1ad2d456a4955661d996c92fa5
Appi Reddy <appireddy.777@outlook.com>
2023-02-26 01:07:45 +0530

    frontend using functions

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,47 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+print_head "Configure Node JS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Install Node JS"
+yum install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop user"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
 mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
 
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "Delete old content"
+rm -rf /app/* &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.devopsar.online </app/schema/catalogue.js
+print_head "Downloading app content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app
+
+print_head "Extracting App Content"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+print_head "Installing NodeJS dependencies "
+npm install &>>${log_file}
+
+print_head "Copy systemD service File"
+cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable catalogue service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start catalogue service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copying mongodb Repo file"
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print_head "Installing MongoDb org shell"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Loading Schema"
+mongo --host mongodb.devopsar.online </app/schema/catalogue.js &>>${log_file}
