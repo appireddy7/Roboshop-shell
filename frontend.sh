f8b62b46359c6070437c816ef930cfc4e6d49588
Appi Reddy <appireddy.777@outlook.com>
2023-02-25 11:47:27 +0530

    copy issue fixed

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+code_dir={pwd}
 echo -e "\e[35mInstalling Nginx\e[0m"
 yum install nginx -y
 echo -e "\e[35mRemoving the old content\e[0m"
@@ -9,7 +10,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mCopying nginx content for roboshop\e[0m"
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp {code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mEnabling Nginx\e[0m"
 systemctl enable nginx
