8daddcaa95ec3fbe3b5a587b13c3ba54b461ea7a
Appi Reddy <appireddy.777@outlook.com>
2023-02-26 10:30:01 +0530

    update mongodb listen address

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,22 @@
 source common.sh
 
 print_head "Installing Nginx"
-yum install nginx -y &>>{log_file}
+yum install nginx -y &>>${log_file}
 print_head "Removing the old content"
-rm -rf /usr/share/nginx/html/* &>>{log_file}
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 print_head "Downloading the Frontend content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{log_file}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 print_head "Extracting downloaded Frontend"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>{log_file}
+unzip /tmp/frontend.zip &>>${log_file}
 
 print_head "Copying nginx content for roboshop"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>{log_file}
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 print_head "Enabling Nginx"
-systemctl enable nginx &>>{log_file}
+systemctl enable nginx &>>${log_file}
 print_head "Starting Nginx"
-systemctl start nginx &>>{log_file}
+systemctl start nginx &>>${log_file}
 
 
 
