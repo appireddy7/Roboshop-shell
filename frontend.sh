22286237ddc43bffa7751d1218aff9a8c561e2e4
Appi Reddy <appireddy.777@outlook.com>
2023-02-27 21:37:23 +0530

    Added if else for testing if else condition

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,11 @@ source common.sh
 
 print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
-echo $?
+if [ $? -eq 0 ]; then
+   echo SUCCESS
+else
+   echo FAILURE
+fi
 print_head "Removing the old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 echo $?
