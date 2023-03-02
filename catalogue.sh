6e0fdcac9e66eede8488ca34da15a15a04eb0b4a
Appi Reddy <appireddy.777@outlook.com>
2023-03-02 11:26:36 +0530

    Added status_check

--- catalogue.sh
+++ catalogue.sh
@@ -14,7 +14,9 @@ fi
 status_check $?
 
 print_head "Create Application Directory"
-mkdir /app
+if [ ! -d /app ]; then
+mkdir /app &>>${log_file
+fi
 status_check $?
 
 print_head "Delete old content"
