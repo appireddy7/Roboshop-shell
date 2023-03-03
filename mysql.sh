d0ef67ada6273cdea43e8f78bc79bdc390b6d95c
Appi Reddy <appireddy.777@outlook.com>
2023-03-03 10:50:56 +0530

    mysql set up

---
+++ mysql.sh
@@ -0,0 +1,26 @@
+source common.sh
+
+my_sql_root_password=$1
+if [ -z "${my_sql_root_password}" ]; then
+  echo -e "\e[31mMissing MySQL Root Password argument\e[0m"
+  exit 1
+  fi
+print_head"Disabling mysql 8 version"
+dnf module disable mysql -y &>>${log_file}
+status_check $?
+
+print_head"Installing MySQL server"
+yum install mysql-community-server -y &>>${log_file}
+status_check $?
+
+print_head"Enable MySQL Service"
+systemctl enable mysqld &>>${log_file}
+status_check $?
+
+print_head"Start MySQL Service"
+systemctl start mysqld &>>${log_file}
+status_check $?
+
+print_head"Set Root Password"
+mysql_secure_installation --set-root-pass ${my_sql_root_password}  &>>${log_file}
+status_check $?
