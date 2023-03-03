ab67e4fe71c0258c46f18e2e8de4395fafe267b1
Appi Reddy <appireddy.777@outlook.com>
2023-03-03 14:02:15 +0530

    rabbitmq setup

---
+++ rabbitmq.sh
@@ -0,0 +1,35 @@
+source common.sh
+
+roboshop_app_password=$1
+if [ -z "${roboshop_app_password}"  ]; then
+  echo -e "\e[31mMissing Roboshop App user password argument\e[0m"
+  exit 1
+  fi
+
+print_head "Setup Erlang Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>${log_file}
+status_check $?
+
+print_head "Setup RabbitMQ Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
+status_check $?
+
+print_head "Install Erlang & RabbitMQ"
+yum install rabbitmq-server erlang -y &>>${log_file}
+status_check $?
+
+print_head "Enable RabbitMQ service"
+systemctl enable rabbitmq-server &>>${log_file}
+status_check $?
+
+print_head "Start RabbitMQ service"
+systemctl start rabbitmq-server &>>${log_file}
+status_check $?
+
+print_head "Add Application user"
+rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+status_check $?
+
+
+print_head "Configure Permission for App user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,6 @@
 source common.sh
 
 mysql_root_password=$1
-
 if [ -z "${mysql_root_password}"  ]; then
   echo -e "\e[31mMissing Mysql Root Password argument\e[0m"
   exit 1
